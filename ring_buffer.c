a0bd92b1e228322a54c32f47884bc00bb1c5770e
Khalyamin Vladimir <vkhalyamin@protonmail.com>
2020-07-23 18:11:13 +0300

    First version (#1)
    * add source files
    
    * Add README.ms
    
    * code style fixes
    
    * ifndef fix
    
    * naming fix
    
    Co-authored-by: Khalyamin Vladimir <vkhalyamin@protonmai.com>

---
+++ ring_buffer.c
@@ -0,0 +1,96 @@
+#include "ring_buffer.h"
+
+int BUFFER_init(RingBuffer *ringBuffer, uint16_t *buffer, int size) {
+    if (!(buffer && size)) {
+        return BUFFER_ERROR;
+    }
+
+    ringBuffer->buffer = buffer;
+    ringBuffer->size = size;
+    BUFFER_flush(ringBuffer);
+
+    return BUFFER_SUCCESS;
+}
+
+int BUFFER_insert(RingBuffer *ringBuffer, uint16_t data) {
+    if ((!ringBuffer) || (!ringBuffer->buffer)) {
+        return BUFFER_ERROR;
+    }
+
+    ringBuffer->buffer[ringBuffer->head] = data;
+    ringBuffer->head = (ringBuffer->head + 1) % ringBuffer->size;
+
+    if (ringBuffer->full) {
+        ringBuffer->tail = (ringBuffer->tail + 1) % ringBuffer->size;
+    }
+
+    ringBuffer->full = (ringBuffer->head == ringBuffer->tail);
+
+    return BUFFER_SUCCESS;
+}
+
+int BUFFER_getFirst(RingBuffer *ringBuffer, uint16_t *data) {
+    if (BUFFER_isEmpty(ringBuffer)) {
+        return BUFFER_ERROR;
+    }
+
+    if (ringBuffer->full) {
+        ringBuffer->full = 0;
+    }
+
+    *data = ringBuffer->buffer[ringBuffer->tail];
+    ringBuffer->tail = (ringBuffer->tail + 1) % ringBuffer->size;
+
+    return BUFFER_SUCCESS;
+}
+
+int BUFFER_getLast(RingBuffer *ringBuffer, uint16_t *data) {
+    if (BUFFER_isEmpty(ringBuffer)) {
+        return BUFFER_ERROR;
+    }
+
+    if (ringBuffer->full) {
+        ringBuffer->full = 0;
+    }
+
+    (ringBuffer->head) ? (ringBuffer->head--) : (ringBuffer->head = ringBuffer->size - 1);
+    *data = ringBuffer->buffer[ringBuffer->head];
+
+    return BUFFER_SUCCESS;
+}
+
+int BUFFER_getCurrentHeadIndex(RingBuffer *ringBuffer) {
+    return ringBuffer->head;
+}
+
+int BUFFER_getCurrentTailIndex(RingBuffer *ringBuffer) {
+    return ringBuffer->tail;
+}
+
+int BUFFER_getSize(RingBuffer *ringBuffer) {
+    return ringBuffer->size;
+}
+
+int BUFFER_getCount(RingBuffer *ringBuffer) {
+    if (ringBuffer->full) {
+        return ringBuffer->size;
+    }
+    else {
+        return (ringBuffer->head >= ringBuffer->tail) ? (ringBuffer->head - ringBuffer->tail)
+            : (ringBuffer->size + ringBuffer->head - ringBuffer->tail);
+    }
+}
+
+int BUFFER_isFull(RingBuffer *ringBuffer) {
+    return ringBuffer->full;
+}
+
+int BUFFER_isEmpty(RingBuffer *ringBuffer) {
+    return ((!ringBuffer->full) && (ringBuffer->head == ringBuffer->tail)); 
+}
+
+void BUFFER_flush(RingBuffer *ringBuffer) {
+    ringBuffer->head = 0;
+    ringBuffer->tail = 0;
+    ringBuffer->full = 0;
+}
---
+++ ring_buffer.h
@@ -0,0 +1,32 @@
+#ifndef RING_BUFFER_H_SENTRY
+#define RING_BUFFER_H_SENTRY
+
+#include <stdint.h>
+#include <stdbool.h>
+
+enum BUFFER_ErrorStatus {
+    BUFFER_SUCCESS,
+    BUFFER_ERROR
+};
+
+typedef struct RingBuffer {
+    uint16_t *buffer;
+    int head;
+    int tail;
+    int size;
+    bool full;
+} RingBuffer;
+
+int BUFFER_init(RingBuffer *ringBuffer, uint16_t *buffer, int size);
+int BUFFER_insert(RingBuffer *ringBuffer, uint16_t data);
+int BUFFER_getFirst(RingBuffer *ringBuffer, uint16_t *data);
+int BUFFER_getLast(RingBuffer *ringBuffer, uint16_t *data);
+int BUFFER_getCurrentHeadIndex(RingBuffer *ringBuffer);
+int BUFFER_getCurrentTailIndex(RingBuffer *ringBuffer);
+int BUFFER_getSize(RingBuffer *ringBuffer);
+int BUFFER_getCount(RingBuffer *ringBuffer);
+int BUFFER_isFull(RingBuffer *ringBuffer);
+int BUFFER_isEmpty(RingBuffer *ringBuffer);
+void BUFFER_flush(RingBuffer *ringBuffer);
+
+#endif
